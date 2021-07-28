87d1c3950d6c501be81a77f20518b4b15037e42e
panas111 <88083247+panas111@users.noreply.github.com>
2021-07-28 12:52:38 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,85 @@
+<fotter>
+
+<div class="fotterImage container-fluid">
+	<div class="mobileFotter fotter container-fluid">
+		<div class="mx-5">
+					<i class="media mb-3 fas fa-map-marker-alt"></i>
+					<p>Mariusz Nowak Computer<br>
+					Center Mar2go!<br>
+					ul. Rynek 32<br>
+					63-900 Rawicz</p>
+		</div>
+				
+	 <div class="mx-3">
+					<i class="media mb-3 fas fa-id-card-alt"></i>
+					<p>Tel: 655457933<br>
+						email: biuro@mar2go.com.pl<br>
+						serwis@mar2go.com.pl
+						</p>
+	 </div>
+				
+	 <div class="mx-3"><h5>MAR2GO ZAPRASZA</h5>
+			<p>PONIEDZIAŁEK - PIĄTEK<br>
+					10:00 - 18:00<br>
+					SOBOTA<br>
+					10:00 - 13:00</p>
+	 </div>
+		
+ </div>
+
+	<div class="mobileFotter2 mx-3"><h5>MAR2GO ZAPRASZA</h5>
+			<p>PONIEDZIAŁEK - PIĄTEK<br>
+				10:00 - 18:00<br>
+				SOBOTA<br>
+				10:00 - 13:00</p>
+	</div>
+</div>
+
+
+<div class="otw container-fluid">
+	<div><p>© Copyright 2021 - Mar2go!</p></div>
+		<div class="mb-2 ml-5">
+			<a href="https://pl-pl.facebook.com/mar2go/" target="_blank">
+			<i class="mr-2 fab fa-facebook-f"></i>
+			</a>
+
+			<a href="https://www.youtube.com/watch?v=wevqyP89Ogk&feature=youtu.be" target="_blank">
+			<i class="mr-2 fab fa-youtube"></i>
+			</a>
+					
+			<a href="https://www.google.com/search?q=mar2go&ei=g9j6YObsEaGvrgTawqzIAg&oq=mar2go&gs_lcp=Cgdnd3Mtd2l6EAMyCwguEMcBEK8BEJMCOg0ILhDHARCvARBDEJMCOgsILhCxAxDHARCjAjoFCAAQsQM6CAgAELEDEIMBOgcIABCxAxBDOgQIABBDOgUILhCxAzoOCC4QsQMQgwEQxwEQrwE6CAguELEDEIMBOgIILjoCCAA6BAgAEB46BggAEAoQHkoECEEYAFDvHViUJWD3JWgAcAB4AIABcIgBvQWSAQMzLjSYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwimuuXRufnxAhWhl4sKHVohCykQ4dUDCBk&uact=5" target="_blank">
+			<i class="fab fa-google-plus"></i>
+			</a>
+		</div>							
+</div>
+
+<div class="otwMobile container-fluid">
+	<div class="mb-2">
+		<a href="https://pl-pl.facebook.com/mar2go/" target="_blank">
+			<i class="mr-2 fab fa-facebook-f"></i>
+		</a>
+		
+		<a href="https://www.youtube.com/watch?v=wevqyP89Ogk&feature=youtu.be" target="_blank">
+			<i class="mr-2 fab fa-youtube"></i>
+		</a>
+			
+		<a href="https://www.google.com/search?q=mar2go&ei=g9j6YObsEaGvrgTawqzIAg&oq=mar2go&gs_lcp=Cgdnd3Mtd2l6EAMyCwguEMcBEK8BEJMCOg0ILhDHARCvARBDEJMCOgsILhCxAxDHARCjAjoFCAAQsQM6CAgAELEDEIMBOgcIABCxAxBDOgQIABBDOgUILhCxAzoOCC4QsQMQgwEQxwEQrwE6CAguELEDEIMBOgIILjoCCAA6BAgAEB46BggAEAoQHkoECEEYAFDvHViUJWD3JWgAcAB4AIABcIgBvQWSAQMzLjSYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwimuuXRufnxAhWhl4sKHVohCykQ4dUDCBk&uact=5" target="_blank">
+			<i class="fab fa-google-plus"></i>
+		</a>
+	</div>
+							
+	<div><p>© Copyright 2021 - Mar2go!</p></div>
+							
+</div>
+</fotter>
+
+<script src="js.js"></script>	
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+  <script src="js.js"></script>
+
+
+
+
