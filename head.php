554cb799a915273783914c1ca3fcd2069ddde4e6
panas111 <88083247+panas111@users.noreply.github.com>
2021-07-29 13:04:19 +0200

    Update head.php

--- head.php
+++ head.php
@@ -9,7 +9,6 @@
 
 <link rel="shortcut icon" href="/favicon.ico">
 
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/ee75fd6a05.js" crossorigin="anonymous"></script>
