989838ec0d1b054d9a6086c1330a0e8f91261190
panas111 <88083247+panas111@users.noreply.github.com>
2021-07-29 12:38:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,86 +17,88 @@
 	
 
 <div class="main container-fluid text-center">
-  <div data-aos="fade-up" class="text"> <p>MAR2GO!</p></div>
-     <div data-aos="fade-up" class="text"><p>SKLEP KOMPUTEROWY</p></div>
-				 <div data-aos="fade-up" class="text">
-          	<a href="#oferta">
-							<button class="text-center btn btn-danger btn-lg" type="button">Nasza oferta</button>	
-						</a>
-          </div>
+  	<div data-aos="fade-up" class="text"> 
+ 		<p>MAR2GO!</p>
+  	</div>
+  	<div data-aos="fade-up" class="text">
+		<p>SKLEP KOMPUTEROWY</p>
+  	</div>	
+  	<div data-aos="fade-up" class="text">
+       		 <a href="#oferta">
+	<button class="text-center btn btn-danger btn-lg" type="button">Nasza oferta</button>	
+		 </a>
+  	</div>
 </div>
 
 	
 <div id="aboutUs" class="backgroundGrey text-center container-fluid">
 	<div class="aboutus mb-5">
-			<p class="text-center">
-				<span class="heading" style="color:#DC143C">Mar2</span> <span class="heading" style="color:black; font-family:Bubblegum Sans">go!</span></p><br>
-				<p class="marginText">Firma powstała w styczniu 2016 r. Zajmujemy się kompleksową obsługą informatyczną firm. Działamy na terenie miast Rawicz, Żmigród, Wąsosz, Pakosław, Bojanowo.
-				Efektywne zarządzanie infrastrukturą IT oraz szybka pomoc użytkownikom to nasze priorytety.  Starannie dobieramy produkty do potrzeb naszych klientów a wszystkie nowości, zanim znajdą się w naszej ofercie są dokładnie sprawdzane pod względem funkcjonalności i niezawodności.</p>
-			</div>
-			</div>	
+		<p class="text-center">
+		<span class="heading" style="color:#DC143C">Mar2</span> <span class="heading" style="color:black; font-family:Bubblegum Sans">go!</span></p><br>
+		<p class="marginText">Firma powstała w styczniu 2016 r. Zajmujemy się kompleksową obsługą informatyczną firm. Działamy na terenie miast Rawicz, Żmigród, Wąsosz, Pakosław, Bojanowo.
+		Efektywne zarządzanie infrastrukturą IT oraz szybka pomoc użytkownikom to nasze priorytety.  Starannie dobieramy produkty do potrzeb naszych klientów a wszystkie nowości, zanim znajdą się w naszej ofercie są dokładnie sprawdzane pod względem funkcjonalności i niezawodności.</p>
+	</div>
+</div>	
 				
 
 
 
 
-	<div class="container">
-		<div  class="whatwedo mt-5">
-			<p class="heading" id="oferta">Co oferujemy</p>
-		</div>
+<div class="container">
+	<div class="whatwedo mt-5">
+		<p class="heading" id="oferta">Co oferujemy</p>
 	</div>
-			<div class="text2 container-fluid my-5 text-center">
-
-
-		<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/time.png"><h1 class="text3">Sprzedaż</h1>
-			<hr>
-			<p class="mt-5">
-				<li>Laptopy</li>
-				<li>Komputery PC</li>
-				<li>Akcesoria komputerowe</li>
-				<li>Kasy fiskalne</li>
-				<li>Oprogramowanie sprzedażowe INSERT / RESET2</li>
-				<li>Oprogramowanie antywirusowe</li>
-			</p>
-		</div>
+</div>	
 	
-		<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/naprawa.png"><h1 class="text3">Serwis</h1>
-			<hr>
-			<p class="mt-5">
-				<li>Naprawa laptopów</li>
-				<li>Naprawa komputerów PC</li>
-				<li>Modernizacja i rozbudowa laptopów / PC</li>
-				<li>Optymalizacja systemu windows</li>
-				<li>Odwirusowanie</li>
-				<li>Pomoc w innych problemach</li>
-			</p>
+<div class="text2 container-fluid my-5 text-center">
+	<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/time.png"><h1 class="text3">Sprzedaż</h1>
+		<hr>
+		<p class="mt-5">
+			<li>Laptopy</li>
+			<li>Komputery PC</li>
+			<li>Akcesoria komputerowe</li>
+			<li>Kasy fiskalne</li>
+			<li>Oprogramowanie sprzedażowe INSERT / RESET2</li>
+			<li>Oprogramowanie antywirusowe</li>
+		</p>
 	</div>
-
-		<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/uslugi.png"><h1 class="text3">Usługi</h1>
-			<hr>
-			<p class="mt-5">
-				<li>Kompleksowa obsługa firm</li>
-				<li>Aktywność w sytuacjach kryzysowych</li>
-				<li>Partnerstwo - stała współpraca</li>
-				<li>Wdrażenie systemów sprzedażowych firmy "INSERT"</li>
-				<li>Wdrażenie programów księgowych</li>
-				<li>Dojazd do klienta</li>
-			</p>
-		</div>
+	<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/naprawa.png"><h1 class="text3">Serwis</h1>
+		<hr>
+		<p class="mt-5">
+			<li>Naprawa laptopów</li>
+			<li>Naprawa komputerów PC</li>
+			<li>Modernizacja i rozbudowa laptopów / PC</li>
+			<li>Optymalizacja systemu windows</li>
+			<li>Odwirusowanie</li>
+			<li>Pomoc w innych problemach</li>
+		</p>
+	</div>
+	<div data-aos="fade-up" class="border"><img class="circle mt-2 mb-2" src="img/uslugi.png"><h1 class="text3">Usługi</h1>
+		<hr>
+		<p class="mt-5">
+			<li>Kompleksowa obsługa firm</li>
+			<li>Aktywność w sytuacjach kryzysowych</li>
+			<li>Partnerstwo - stała współpraca</li>
+			<li>Wdrażenie systemów sprzedażowych firmy "INSERT"</li>
+			<li>Wdrażenie programów księgowych</li>
+			<li>Dojazd do klienta</li>
+		</p>
+	</div>
 </div>
 
 
 <div id="kasyFiskalne" class="backgroundGrey container-fluid text-center ">
 	<div class="aboutus my-3">
-				<p class="text-center">
-				<span class="heading" style="color:#DC143C; font-family:Bubblegum Sans">Kasy-</span> <span class="heading" style="color:black; font-family:Bubblegum Sans" >fiskalne</span></p>
-				<p class="marginText">Drukarki i kasy fiskalne to kluczowe produkty marki Novitus, które od 25 lat wspierają tysiące polskich przedsiębiorców. Najwyższa jakość, znakomite wzornictwo i innowacyjne rozwiązania technologiczne oferty Novitus zostały wielokrotnie potwierdzone wyróżnieniami Lidera Rynku, Godła Promocyjnego „Teraz Polska”, czy tytułami: Dobry Wzór, Innowacja w handlu czy Złoty Paragon.</p>
-
+		<p class="text-center">
+		<span class="heading" style="color:#DC143C; font-family:Bubblegum Sans">Kasy-</span> <span class="heading" style="color:black; font-family:Bubblegum Sans" >fiskalne</span></p>
+		<p class="marginText">Drukarki i kasy fiskalne to kluczowe produkty marki Novitus, które od 25 lat wspierają tysiące polskich przedsiębiorców. 
+		Najwyższa jakość, znakomite wzornictwo i innowacyjne rozwiązania technologiczne oferty Novitus zostały wielokrotnie potwierdzone wyróżnieniami
+		Lidera Rynku, Godła Promocyjnego „Teraz Polska”, czy tytułami: Dobry Wzór, Innowacja w handlu czy Złoty Paragon.
+		</p>
 		<div class="border2">
-			<img src="img/nano3.jpg" class="nano3 img-fluid">
+		<img src="img/nano3.jpg" class="nano3 img-fluid">
 		</div>
-			<a href="https://novitus.pl/index.php/urzadzenia-fiskalne" target="blank"><button class="text-center btn btn-danger btn-md" type="button">Zobacz więcej</button></a>
-
+		<a href="https://novitus.pl/index.php/urzadzenia-fiskalne" target="blank"><button class="text-center btn btn-danger btn-md" type="button">Zobacz więcej</button></a>
 	</div>
 </div>
 
